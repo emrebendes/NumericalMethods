448991f47ad32ed1943971295178a60afa5a54dc
emrebendes <93640625+emrebendes@users.noreply.github.com>
2023-10-14 19:33:42 +0300

    Update hatalar.py

--- 2.Hafta/hatalar.py
+++ 2.Hafta/hatalar.py
@@ -4,10 +4,10 @@ import math
 import matplotlib.pyplot as plt
 
 
-def f(x):
+def f(x): #e^-1+ln(x) fonksiyonu
     return np.power(math.e,-1*x)+np.log(x)
 
-def plot(n0,n1):
+def plot(n0,n1):# verilen aralıkta f(x) fonksiyonunu 100 değer ile çalıştırıp o aralıkta grafik çiziyor.
     x=np.linspace(n0,n1,101)
     s= f(x)
     plt.plot(x,s)
@@ -15,7 +15,7 @@ def plot(n0,n1):
     plt.xticks(np.arange(n0,n1,(n1-n0)/10))
     plt.show()
 
-def bul():
+def bul():#0-10 aralığında fonksiyonun nerede işret değiştirdiğini arıyor.
     x=np.linspace(0,10,101)
     y= f(x)
     limits=np.array([[0,10]])
@@ -30,13 +30,15 @@ def bul():
         y= f(x)
     return limits
  
-def ex(x,n):
+def ex(x,n):#maclourin açılımını x değeri için n elemanla hesaplıyor.
     # s=0;
     # for i in range(n):
     #     s+=x**i/math.factorial(i)
     # return s
     return sum([x**i/math.factorial(i) for i in range(n)])
 
+
+# 10 iterasyon maclourin açılımı açıp gerçek ve yaklaşık hataları hesaplayarak tablo hlinde ekrana bastır
 print("   Sonuç \t\t\t Et \t\t\t Ea") 
 snc=[];
 gd=math.e**0.5
@@ -48,5 +50,7 @@ for i in range(1,10):
         print("%d - %1.8f \t %1.8f \t %1.8f" % (i,snc[-1],et,ea) ) 
        
 # ex(2)
-limit=bul()    
+# limit=bul()   
+
+#  e^-1+ln(x) fonksiyonunu farklı aralıklarda deneyerek grafiğini çiz ve sürekli bir hata olduğunu gözlemle ...
 plot(.5,.6)
